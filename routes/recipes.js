e19197f171ba31086cca76a32479e54c1bcdcc19
Okta P <57696739+oktaaaa@users.noreply.github.com>
2025-01-13 06:43:36 +0700

    recipe crud ok

--- routes/recipes.js
+++ routes/recipes.js
@@ -19,10 +19,46 @@ router.post("/create", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const recipe = await Recipe.find().populate("categoryId")
+    const recipe = await Recipe.find().populate("categoryId");
     res.json(recipe);
   } catch (error) {
     res.json({ message: error });
+  }
+});
+
+router.delete("/:ID", async (req, res) => {
+  try {
+    const recipe = await Recipe.deleteOne({
+      _id: req.params.ID,
+    });
+    res.json(recipe);
+  } catch (error) {
+    res.json({
+      message: error,
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      message: "Recipe Updated",
+      Data: recipe,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    res.status(200).json(recipe);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
